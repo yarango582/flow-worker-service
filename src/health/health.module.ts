5f0370a103e88ca3651d70664b3f0a41a7d6e1f7
Yeison Arango <yeisonarango03@gmail.com>
2025-08-03 23:01:19 -0500

    bug solve

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '../config/config.module';
 import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
 
 @Module({
+  imports: [ConfigModule],
   providers: [HealthService],
-  controllers: [HealthController]
+  controllers: [HealthController],
+  exports: [HealthService],
 })
 export class HealthModule {}
